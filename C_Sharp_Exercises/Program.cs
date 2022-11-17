3bca755f4e9a1669ff90b14f7ee24adc39ac2f06
atefeh97hmt <hemmati.atefeh97@gmail.com>
2022-11-17 17:00:04 +0330

    Add two numbers that user enter

--- C_Sharp_Exercises/Program.cs
+++ C_Sharp_Exercises/Program.cs
@@ -21,19 +21,38 @@ using System.Threading.Tasks;
 //}
 
 ////////////////////////Add two numbers///////////////////////////
+//namespace MyExercises
+//{
+//    class sum
+//    {
+//        static void Main()
+//        {
+//            int a = 100;
+//            int b = 60;
+//            int sum = a + b;
+//            Console.WriteLine(sum);
+//            Console.ReadKey();
+
+//        }
+//    }
+//}
+
+////////////////////////Add two numbers that user enter///////////////////////////
 namespace MyExercises
 {
     class sum
     {
         static void Main()
         {
-            int a = 100;
-            int b = 60;
-            int sum = a + b;
-            Console.WriteLine(sum);
+            //int number1;
+            //int number2;
+            Console.WriteLine("Please enter the number1");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the number2");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int result = number1 + number2;
+            Console.WriteLine("sum of two numbers:" + result.ToString());
             Console.ReadKey();
-
         }
     }
 }
-
