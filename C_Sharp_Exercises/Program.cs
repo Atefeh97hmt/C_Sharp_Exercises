63047557e017704bd2634e57b03c48aa712bdd6b
atefeh97hmt <hemmati.atefeh97@gmail.com>
2022-11-17 17:27:28 +0330

    average of n user input numbers

--- C_Sharp_Exercises/Program.cs
+++ C_Sharp_Exercises/Program.cs
@@ -86,33 +86,65 @@ using System.Threading.Tasks;
 //}
 
 ////////////////////////average of five numbers that user enter with loop///////////////////////////
+//namespace C_Sharp_Exercises
+//{
+
+
+//    class average
+//    {
+//        static void Main()
+//        {
+
+//            int[] number = new int[5];
+//            for( int i=0; i < number.Length; i++)
+//            {
+//                Console.WriteLine("please enter a number:");
+//                number[i] = Convert.ToInt32(Console.ReadLine());
+//            }
+
+//            int sum = 0;
+//            for (int i = 0; i < number.Length; ++i)
+//            {
+//                sum += number[i];
+//            }
+
+//            Console.WriteLine("average of five numbers is: {0}", sum / number.Length);
+//            Console.ReadKey();
+
+//        }
+
+//    }
+
+//}
+
+
+////////////////////////average of n user input numbers///////////////////////////
 namespace C_Sharp_Exercises
 {
 
 
     class average
     {
-        static void Main()
+        static void Main(string[] args)
         {
+            int Total, Sum = 0;
+            int Average;
 
-            int[] number = new int[5];
-            for( int i=0; i < number.Length; i++)
+            Console.WriteLine("How many numbers do you want the average of? ");
+            Total = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < Total; i++)
             {
-                Console.WriteLine("please enter a number:");
-                number[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter a number: ");
+                int Value = Convert.ToInt32(Console.ReadLine());
+                Sum += Value;
             }
 
-            int sum = 0;
-            for (int i = 0; i < number.Length; ++i)
-            {
-                sum += number[i];
-            }
+            Average = Sum / Total;
 
-            Console.WriteLine("average of five numbers is: {0}", sum / number.Length);
+            Console.WriteLine("The average is: " + Average);
             Console.ReadKey();
-
         }
-
     }
 
 }
